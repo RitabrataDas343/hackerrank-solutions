bebeae17b15a3b0159f9e4d0e9a94cddd369a94d
Ritabrata Das <dasritabrata5@gmail.com>
2022-11-04 19:35:14 +0530

    added recent solutions

---
+++ problem-solving/Big Sorting.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+bool compare(string a, string b){
+    if(a.size() == b.size()){
+        return a < b;
+    } else {
+        return a.size() < b.size();
+    }
+}
+
+vector<string> bigSorting(vector<string> unsorted) {
+    sort(unsorted.begin(), unsorted.end(), compare);
+    return unsorted;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<string> unsorted(n);
+
+    for (int i = 0; i < n; i++) {
+        string unsorted_item;
+        getline(cin, unsorted_item);
+
+        unsorted[i] = unsorted_item;
+    }
+
+    vector<string> result = bigSorting(unsorted);
+
+    for (size_t i = 0; i < result.size(); i++) {
+        fout << result[i];
+
+        if (i != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
